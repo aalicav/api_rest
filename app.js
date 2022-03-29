da1d51a577b05c365adc9dcc173e61ae4dbe9247
aalicav <antonioalissoncavalcantedeoliv@gmail.com>
2022-03-29 18:07:15 -0300

    correção user/use

--- app.js
+++ app.js
@@ -5,11 +5,27 @@ import './src/config/database';
 import express from 'express';
 import { resolve } from 'path';
 
+import cors from 'cors';
+import helmet from 'helmet';
+
 import homeRoutes from './src/routes/homeRoutes';
 import userRoutes from './src/routes/userRoutes';
 import tokenRoutes from './src/routes/tokenRoutes';
 import alunoRoutes from './src/routes/alunoRoutes';
 import photoRoutes from './src/routes/photoRoutes';
+
+const whiteList = {
+};
+
+const corsOptions = {
+  origin(origin, cb) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      cb(null, true);
+    } else {
+      cb(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -19,6 +35,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors());
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
